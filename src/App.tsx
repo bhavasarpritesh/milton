d1236e66b2fd507c34dadd0b5ee722a42c34579e
pritesh bhavsar <pritesh@bbd.co.za>
2024-01-24 19:44:22 +0530

    fix: added nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './navbar/navbar';
+import logo from "/logo.svg";
 
 function App() {
+  let items = ["Home","Product", "Service"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav 
+      brandName="My Brand" 
+      imageSrcPath="" 
+      navItems={items}/>
     </div>
   );
 }
